a2c167a47e36ccf3bd03db5ef080ef4021733a1d
Clanbulance <107610779+Clanbulance@users.noreply.github.com>
2025-08-07 23:28:30 +0200

    push

---
+++ src/Hello.jsx
@@ -0,0 +1,26 @@
+function HelloThere(){
+    return (
+        <>
+         <h1>Hello There youngling!</h1>
+         <p>Click on the button below to see grazy shit</p>
+
+        </>
+    )
+}
+
+
+function Button(){
+    const pic = ''
+    return (
+        <div className="button1">
+            <img 
+                src="" 
+                alt="" />
+            <button id="bangerbutton">NOSECLICKER</button>
+        </div>
+    )
+}
+
+
+
+export {HelloThere, Button}
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import {HelloThere,Button} from './Hello.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <HelloThere />
+    <Button />
   </StrictMode>,
 )
